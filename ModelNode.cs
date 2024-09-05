e9b54e0012f92eef1ad77bc2e21f9174ce4c0ae5
chezkyk <Y0548532123@GMAIL.COM>
2024-09-05 13:01:33 +0300

    A helper model for reading the JSON file

---
+++ ModelNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinariTreeTest
+{
+    public class ModelNode
+    {
+        public int MaxSeverity { get; set; }
+        public int MinSeverity { get; set; }
+        public List<string> Defenses { get; set; }
+    }
+}
