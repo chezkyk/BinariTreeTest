f9a67b46a8f08ed988e79a1c354b4127d896af05
chezkyk <Y0548532123@GMAIL.COM>
2024-09-05 13:15:42 +0300

    Added Insert function

---
+++ DefenceStrategiesBST.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinariTreeTest
+{
+    public class DefenceStrategiesBST
+    {
+        public Node root;
+
+        public DefenceStrategiesBST()
+        {
+            root = null;
+        }
+
+        public void Insert(ModelNode value)
+        {
+            root = InsertRecursion(root, value);
+
+        }
+        private Node InsertRecursion(Node node, ModelNode value)
+        {
+            if (node == null)
+            {
+                return new Node(value);
+            }
+            if (value.MinSeverity < node.Value.MinSeverity)
+            {
+                node.Left = InsertRecursion(node.Left, value);
+            }
+            else if (value.MinSeverity > node.Value.MinSeverity)
+            {
+                node.Right = InsertRecursion(node.Right, value);
+            }
+            return node;
+        }
+        
+    }
+}
