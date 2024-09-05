19a626c53b267c86ff20c4b58e8f88a1adaaad31
chezkyk <Y0548532123@GMAIL.COM>
2024-09-05 16:37:35 +0300

    Added all models into a folder to keep the project in nice order

---
+++ Models/ModelThreat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinariTreeTest.Models
+{
+    public class ModelThreat
+    {
+        public string ThreatType { get; set; }
+        public int Volume { get; set; }
+        public int Sophistication { get; set; }
+        public string Target { get; set; }
+
+    }
+}
---
+++ Models/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinariTreeTest.Models
+{
+    public class Node
+    {
+        public Node Right { get; set; }
+        public Node Left { get; set; }
+
+        public ModelNode Value { get; set; }
+
+        public Node(ModelNode modelNode)
+        {
+            Value = modelNode;
+            Right = null;
+            Left = null;
+        }
+
+    }
+}
